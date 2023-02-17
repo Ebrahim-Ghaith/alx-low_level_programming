ef10bc1723feeee05e147123ff9b0937df849777
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-02-17 22:28:15 +0200

    test

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * File: 4-puts.c
+ * Auth: Ebrahim Ghaith  <Ebrahimghaith7@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main--> Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ * file : 5-printf.c
+ * Auth : Ebrahim Ghaith <Ebrahimghaith7@gmail.com>
+ */
+
+#include <stdio.h>
+
+/*
+ * main --> prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+} 
