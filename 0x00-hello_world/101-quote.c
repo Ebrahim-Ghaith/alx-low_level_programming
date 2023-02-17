ac75cbf1fdc2acd04d4c3476004c418f36c0316c
Ebrahim-Ghaith <84124169+Ebrahim-Ghaith@users.noreply.github.com>
2023-02-17 23:03:34 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
 #include <unistd.h>
 
 /**
- * main --> prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,
- *          followed by a new line, to the standard error.
+ * main --> prints exactly and that piece of art is useful" - Dora Korpar
+ *         2015-10-19 followed by a new line, to the standard error.
  * Return: always 1.
  */
 int main(void)
