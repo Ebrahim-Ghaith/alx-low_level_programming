21530c10c043a686ed10dc945fda3bfae913e766
Ebrahim-Ghaith <84124169+Ebrahim-Ghaith@users.noreply.github.com>
2023-03-08 00:05:33 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
   * main :  fills the first n bytes of the memory area
   * @s: memory area to fill
@@ -13,9 +12,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
+	
 	for (a = 0;a < n; a++)
 	{
 	s[a]=b;
 	}
+	
 	return (s);
 }
