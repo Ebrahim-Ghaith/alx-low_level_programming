dac61b27c32d553a3e09b66c170f18fa14eff39e
Ebrahim-Ghaith <84124169+Ebrahim-Ghaith@users.noreply.github.com>
2023-03-15 17:40:48 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,11 +8,9 @@
   *
   * Return: Always zero
   */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	(void) argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
 }
-
