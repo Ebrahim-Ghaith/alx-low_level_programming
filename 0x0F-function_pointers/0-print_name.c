03508f41520198a5167948680598c6d6b751194d
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-03-23 11:32:56 +0200

    test

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 /**
  * print_name : print a name
  * @name : name to printed
- *
+ * @f : pointer to func tthat prints name
  * returns : nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if(name == NULL || f == NULL)
-		return:
+		return;
 	f(name);
 }
