4df9af8fe85437baa92522424c710ce8bcdfac46
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-04-11 20:45:32 +0200

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,23 +4,22 @@
 /**
  * binary_to_uint - converts a binary number
  *		    to an unsigned int
+ * @b: input
  * Return: converted number
  */
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int result = 0;
 
-unsigned int result = 0;
-    if (b == NULL)
-        return 0;
-    for (; *b != '\0'; b++) {
-        if (*b == '0' || *b == '1') {
-            result = (result << 1) | (*b - '0');
-        } else {
-            return 0;
-        }
-    }
-    return result;
-
-
+	if (b == NULL)
+		return (0);
+	for (; *b != '\0'; b++)
+	{
+		if (*b == '0' || *b == '1')
+			result = (result << 1) | (*b - '0');
+		else
+			return (0);
+	}
+	return result;
 }
