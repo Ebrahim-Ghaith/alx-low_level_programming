505dff2364e1e7c07aac29d59f3ae4b94c569efb
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-04-20 17:33:49 +0200

    test

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all digit num from 0 to 9 with ,
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int x = 48;
+	int y = 44;
+	int z = 32;
+
+	while (x < 58)
+	{
+		if (x == 57)
+		{
+			putchar(x);
+		}
+		else
+		{
+			putchar(x);
+			putchar(y);
+			putchar(z);
+		}
+		x++;
+	}
+	return (0);
+}
