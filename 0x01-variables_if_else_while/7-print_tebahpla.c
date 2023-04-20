b69927d6ba5539e74af3de6896f54ef5e28bee4d
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-04-20 17:14:46 +0200

    test

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all digit num from 0 to 9
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int x = 122;
+
+	while (x > 96)
+	{
+		putchar(x);
+		--x;
+	}
+	putchar('\n');
+	return (0);
+}
