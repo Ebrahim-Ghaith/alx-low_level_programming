1f3ed6d6ca047ac8cbf94799365de408fe1c652d
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-04-20 16:53:42 +0200

    test

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all digit num from 0 to 9
+ * Return: nothing
+ */
+
+int main()
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
