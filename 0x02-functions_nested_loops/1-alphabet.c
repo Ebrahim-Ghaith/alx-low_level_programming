13c31ace4924d78ede7d7af799eff25a9956d149
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-04-21 20:26:47 +0200

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ * return: nothing
+ */
+
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
