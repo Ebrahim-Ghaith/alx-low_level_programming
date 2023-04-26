cba1ddeb7f30f2d856302fdf9bb230f57f72dd66
Ebrahim Ghaith <ebrahimghaith7@gmail.com>
2023-04-26 19:46:02 +0200

    test

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts2 - prints a 2 char string, followed by a new line
+ * @str: input string
+ * Return: nothing
+ */
+
+
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len])
+	{
+		if (len % 2 == 0)
+		{
+			_putchar(str[len]);
+		}
+		len++;
+	}
+	_putchar('\n');
+}
